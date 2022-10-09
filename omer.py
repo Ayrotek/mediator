ccc424002e6d71bfc8076ccea64e87b25ee59713
Batuhan Özcan <batuhan.ozcan97@gmail.com>
2022-10-09 23:24:58 +0300

    Create omer.py

---
+++ omer.py
@@ -0,0 +1,1 @@
+aca-py start --inbound-transport http 0.0.0.0 8000 --genesis-url http://test.bcovrin.vonx.io/genesis --inbound-transport ws 0.0.0.0 8001 --outbound-transport ws --outbound-transport http --endpoint http://localhost:8000
