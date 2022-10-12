c553a9866d3e311f63355202342e8857061d2ff7
Batuhan Özcan <batuhan.ozcan97@gmail.com>
2022-10-12 21:25:08 +0300

    Update omer.py

--- omer.py
+++ omer.py
@@ -1,1 +1,3 @@
+chmod a+x setup.py
+./setup.py run build
 aca-py start --inbound-transport http 0.0.0.0 8000 --genesis-url http://test.bcovrin.vonx.io/genesis --inbound-transport ws 0.0.0.0 8001 --outbound-transport ws --outbound-transport http --endpoint http://localhost:8000
