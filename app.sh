d08e38b831564e1521bcc5dda8910f9d1db12ca3
Batuhan Özcan <batuhan.ozcan97@gmail.com>
2022-10-16 03:35:15 +0300

    Delete app.sh

--- app.sh
+++
@@ -1,1 +0,0 @@
-aca-py start --inbound-transport http 0.0.0.0 8000 --genesis-url http://test.bcovrin.vonx.io/genesis --inbound-transport ws 0.0.0.0 8001 --outbound-transport ws --outbound-transport http --endpoint http://localhost:8000
